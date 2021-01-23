49c9d47c9b18d483254fd1302457af90330255c7
InnerPulse <stoev95pd@gmail.com>
2021-01-23 08:28:06 -0800

    Update 08_MinerTask.js

--- 07 Associative Arrays/08_MinerTask.js
+++ 07 Associative Arrays/08_MinerTask.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:07 Associative Arrays/08_MinerTask.js
 function foo(arr) {
     let result = {}
     for (let i = 0; i < arr.length; i += 1) {
@@ -11,17 +10,3 @@ function foo(arr) {
 
     Object.entries(result).forEach(x => console.log(`${x[0]} -> ${x[1]}`))
 }
-=======
-function foo(arr) {
-    let result = {}
-    for (let i = 0; i < arr.length; i += 1) {
-        if (i % 2 === 0) {
-            if (result[arr[i]] === undefined) result[arr[i]] = 0
-        } else {
-            result[arr[i - 1]] += Number(arr[i])
-        }
-    }
-
-    Object.entries(result).forEach(x => console.log(`${x[0]} -> ${x[1]}`))
-}
->>>>>>> 67fe0501e121141605e4d373d81b6af86bc463d4:07 Associative Arrays/08_aMinerTask.js
