4c76afe406007cc712b401c9305a055575893004
InnerPulse <stoev95pd@gmail.com>
2021-01-23 08:16:28 -0800

    Update 04_MetersToKilometers.js

--- 02 Data Types and Variables/04_MetersToKilometers.js
+++ 02 Data Types and Variables/04_MetersToKilometers.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:02 Data Types and Variables/04_MetersToKilometers.js
 function foo (x) {
     return (x / 1000).toFixed(2)
 }
@@ -6,4 +5,3 @@ function foo (x) {
 function foo (x) {
     return (x / 1000).toFixed(2)
 }
->>>>>>> 67fe0501e121141605e4d373d81b6af86bc463d4:02 Data Types and Variables/04_coonvertMetersToKilometers.js
